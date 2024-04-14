8d1c869a373681e3ca1f1367c4db87378858d200
Britannio <britanniojarrett@gmail.com>
2024-04-14 22:24:15 +0100

    remove forward declarations

--- crsf.c
+++ crsf.c
@@ -31,18 +31,6 @@
 #define DEBUG_INFO(...)
 #endif
 
-uint8_t crsf_crc8(const uint8_t *ptr, uint8_t len);
-void sb_reset(stream_buffer_t *sbuf);
-void sb_write_ui8(stream_buffer_t *sbuf, uint8_t data);
-void sb_write_i8(stream_buffer_t *sbuf, int8_t data);
-void sb_write_ui16(stream_buffer_t *sbuf, uint16_t data);
-void sb_write_i16(stream_buffer_t *sbuf, int16_t data);
-void sb_write_ui24(stream_buffer_t *sbuf, uint32_t data);
-void sb_write_i24(stream_buffer_t *sbuf, int32_t data);
-void sb_write_ui32(stream_buffer_t *sbuf, uint32_t data);
-void sb_write_i32(stream_buffer_t *sbuf, int32_t data);
-bool crsf_telem_update();
-
 uart_inst_t *_uart;
 uint8_t _incoming_frame[CRSF_MAX_FRAME_SIZE];
 uint16_t _rc_channels[CRSF_MAX_CHANNELS];
@@ -204,6 +192,192 @@ bool calculate_failsafe()
   return _link_statistics.link_quality <= _link_quality_threshold || _link_statistics.rssi >= _rssi_threshold;
 }
 
+uint8_t crsf_crc8(const uint8_t *ptr, uint8_t len)
+{
+  static const uint8_t crsf_crc8tab[256] = {
+      0x00, 0xD5, 0x7F, 0xAA, 0xFE, 0x2B, 0x81, 0x54, 0x29, 0xFC, 0x56, 0x83, 0xD7, 0x02, 0xA8, 0x7D,
+      0x52, 0x87, 0x2D, 0xF8, 0xAC, 0x79, 0xD3, 0x06, 0x7B, 0xAE, 0x04, 0xD1, 0x85, 0x50, 0xFA, 0x2F,
+      0xA4, 0x71, 0xDB, 0x0E, 0x5A, 0x8F, 0x25, 0xF0, 0x8D, 0x58, 0xF2, 0x27, 0x73, 0xA6, 0x0C, 0xD9,
+      0xF6, 0x23, 0x89, 0x5C, 0x08, 0xDD, 0x77, 0xA2, 0xDF, 0x0A, 0xA0, 0x75, 0x21, 0xF4, 0x5E, 0x8B,
+      0x9D, 0x48, 0xE2, 0x37, 0x63, 0xB6, 0x1C, 0xC9, 0xB4, 0x61, 0xCB, 0x1E, 0x4A, 0x9F, 0x35, 0xE0,
+      0xCF, 0x1A, 0xB0, 0x65, 0x31, 0xE4, 0x4E, 0x9B, 0xE6, 0x33, 0x99, 0x4C, 0x18, 0xCD, 0x67, 0xB2,
+      0x39, 0xEC, 0x46, 0x93, 0xC7, 0x12, 0xB8, 0x6D, 0x10, 0xC5, 0x6F, 0xBA, 0xEE, 0x3B, 0x91, 0x44,
+      0x6B, 0xBE, 0x14, 0xC1, 0x95, 0x40, 0xEA, 0x3F, 0x42, 0x97, 0x3D, 0xE8, 0xBC, 0x69, 0xC3, 0x16,
+      0xEF, 0x3A, 0x90, 0x45, 0x11, 0xC4, 0x6E, 0xBB, 0xC6, 0x13, 0xB9, 0x6C, 0x38, 0xED, 0x47, 0x92,
+      0xBD, 0x68, 0xC2, 0x17, 0x43, 0x96, 0x3C, 0xE9, 0x94, 0x41, 0xEB, 0x3E, 0x6A, 0xBF, 0x15, 0xC0,
+      0x4B, 0x9E, 0x34, 0xE1, 0xB5, 0x60, 0xCA, 0x1F, 0x62, 0xB7, 0x1D, 0xC8, 0x9C, 0x49, 0xE3, 0x36,
+      0x19, 0xCC, 0x66, 0xB3, 0xE7, 0x32, 0x98, 0x4D, 0x30, 0xE5, 0x4F, 0x9A, 0xCE, 0x1B, 0xB1, 0x64,
+      0x72, 0xA7, 0x0D, 0xD8, 0x8C, 0x59, 0xF3, 0x26, 0x5B, 0x8E, 0x24, 0xF1, 0xA5, 0x70, 0xDA, 0x0F,
+      0x20, 0xF5, 0x5F, 0x8A, 0xDE, 0x0B, 0xA1, 0x74, 0x09, 0xDC, 0x76, 0xA3, 0xF7, 0x22, 0x88, 0x5D,
+      0xD6, 0x03, 0xA9, 0x7C, 0x28, 0xFD, 0x57, 0x82, 0xFF, 0x2A, 0x80, 0x55, 0x01, 0xD4, 0x7E, 0xAB,
+      0x84, 0x51, 0xFB, 0x2E, 0x7A, 0xAF, 0x05, 0xD0, 0xAD, 0x78, 0xD2, 0x07, 0x53, 0x86, 0x2C, 0xF9};
+
+  uint8_t crc = 0;
+  for (uint8_t i = 0; i < len; i++)
+  {
+    crc = crsf_crc8tab[crc ^ *ptr++];
+  }
+  return crc;
+}
+
+void sb_reset(stream_buffer_t *sbuf)
+{
+  if (sbuf)
+  {
+    sbuf->offset = 0;
+  }
+}
+
+// Write an uint8_t to the buffer
+void sb_write_ui8(stream_buffer_t *sbuf, uint8_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(uint8_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = data;
+    sbuf->offset += sizeof(uint8_t);
+  }
+}
+
+// Write an int8_t to the buffer
+void sb_write_i8(stream_buffer_t *sbuf, int8_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(int8_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = data;
+    sbuf->offset += sizeof(int8_t);
+  }
+}
+
+// Write an uint16_t to the buffer
+void sb_write_ui16(stream_buffer_t *sbuf, uint16_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(uint16_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = data & 0xFF;
+    sbuf->offset += sizeof(uint16_t);
+  }
+}
+
+// Write an int16_t to the buffer
+void sb_write_i16(stream_buffer_t *sbuf, int16_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(int16_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = data & 0xFF;
+    sbuf->offset += sizeof(int16_t);
+  }
+}
+
+// Write an uint24_t to the buffer
+void sb_write_ui24(stream_buffer_t *sbuf, uint32_t data)
+{
+  if (sbuf && (sbuf->offset + 3 <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 16) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 2] = data & 0xFF;
+    sbuf->offset += 3;
+  }
+}
+
+// Write an int24_t to the buffer
+void sb_write_i24(stream_buffer_t *sbuf, int32_t data)
+{
+  if (sbuf && (sbuf->offset + 3 <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 16) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 2] = data & 0xFF;
+    sbuf->offset += 3;
+  }
+}
+
+// Write an uint32_t to the buffer
+void sb_write_ui32(stream_buffer_t *sbuf, uint32_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(uint32_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 24) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = (data >> 16) & 0xFF;
+    sbuf->buffer[sbuf->offset + 2] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 3] = data & 0xFF;
+    sbuf->offset += sizeof(uint32_t);
+  }
+}
+
+// Write an int32_t to the buffer
+void sb_write_i32(stream_buffer_t *sbuf, int32_t data)
+{
+  if (sbuf && (sbuf->offset + sizeof(int32_t) <= sbuf->capacity))
+  {
+    sbuf->buffer[sbuf->offset] = (data >> 24) & 0xFF;
+    sbuf->buffer[sbuf->offset + 1] = (data >> 16) & 0xFF;
+    sbuf->buffer[sbuf->offset + 2] = (data >> 8) & 0xFF;
+    sbuf->buffer[sbuf->offset + 3] = data & 0xFF;
+    sbuf->offset += sizeof(int32_t);
+  }
+}
+
+void _begin_frame()
+{
+  sb_reset(&_telemBuf);
+  // Write sync byte
+  sb_write_ui8(&_telemBuf, 0xC8);
+}
+
+void _end_frame()
+{
+  // Skip sync byte and frame length
+  const uint8_t bytesToSkip = 2;
+  const uint8_t *start = _telemBuf.buffer + bytesToSkip;
+  const uint8_t length = _telemBuf.offset - bytesToSkip;
+  const uint8_t crc = crsf_crc8(start, length);
+
+  sb_write_ui8(&_telemBuf, crc);
+}
+
+// BEGIN gen_frames.dart
+void _write_battery_sensor_payload()
+{
+  sb_write_ui8(&_telemBuf, 10);                            // Frame length
+  sb_write_ui8(&_telemBuf, CRSF_FRAMETYPE_BATTERY_SENSOR); // Frame type
+  sb_write_ui16(&_telemBuf, _telemetry.battery_sensor.voltage);
+  sb_write_ui16(&_telemBuf, _telemetry.battery_sensor.current);
+  sb_write_ui24(&_telemBuf, _telemetry.battery_sensor.capacity);
+  sb_write_ui8(&_telemBuf, _telemetry.battery_sensor.percent);
+}
+// END gen_frames.dart
+
+bool crsf_telem_update()
+{
+  bool updated = false;
+  static int currentFrameType = 0;
+
+  for (int i = 0; i < TELEMETRY_FRAME_TYPES; i++)
+  {
+    int frameTypeIndex = (currentFrameType + i) % TELEMETRY_FRAME_TYPES;
+
+    if (frameHasData[frameTypeIndex])
+    {
+      _begin_frame();
+      switch (frameTypeIndex)
+      {
+      case CRSF_BATTERY_INDEX:
+        _write_battery_sensor_payload();
+        break;
+      }
+      _end_frame();
+      updated = true;
+      currentFrameType = (currentFrameType + 1) % TELEMETRY_FRAME_TYPES;
+      break;
+    }
+  }
+
+  return updated;
+}
+
 /**
  * @brief Processes incoming CRSF frames.
  *
@@ -316,64 +490,6 @@ void crsf_process_frames()
   }
 }
 
-void _begin_frame()
-{
-  sb_reset(&_telemBuf);
-  // Write sync byte
-  sb_write_ui8(&_telemBuf, 0xC8);
-}
-
-void _end_frame()
-{
-  // Skip sync byte and frame length
-  const uint8_t bytesToSkip = 2;
-  const uint8_t *start = _telemBuf.buffer + bytesToSkip;
-  const uint8_t length = _telemBuf.offset - bytesToSkip;
-  const uint8_t crc = crsf_crc8(start, length);
-
-  sb_write_ui8(&_telemBuf, crc);
-}
-
-// BEGIN gen_frames.dart
-void _write_battery_sensor_payload()
-{
-  sb_write_ui8(&_telemBuf, 10);                            // Frame length
-  sb_write_ui8(&_telemBuf, CRSF_FRAMETYPE_BATTERY_SENSOR); // Frame type
-  sb_write_ui16(&_telemBuf, _telemetry.battery_sensor.voltage);
-  sb_write_ui16(&_telemBuf, _telemetry.battery_sensor.current);
-  sb_write_ui24(&_telemBuf, _telemetry.battery_sensor.capacity);
-  sb_write_ui8(&_telemBuf, _telemetry.battery_sensor.percent);
-}
-// END gen_frames.dart
-
-bool crsf_telem_update()
-{
-  bool updated = false;
-  static int currentFrameType = 0;
-
-  for (int i = 0; i < TELEMETRY_FRAME_TYPES; i++)
-  {
-    int frameTypeIndex = (currentFrameType + i) % TELEMETRY_FRAME_TYPES;
-
-    if (frameHasData[frameTypeIndex])
-    {
-      _begin_frame();
-      switch (frameTypeIndex)
-      {
-      case CRSF_BATTERY_INDEX:
-        _write_battery_sensor_payload();
-        break;
-      }
-      _end_frame();
-      updated = true;
-      currentFrameType = (currentFrameType + 1) % TELEMETRY_FRAME_TYPES;
-      break;
-    }
-  }
-
-  return updated;
-}
-
 /**
  * Sets the battery data in the telemetry structure.
  *
@@ -389,132 +505,4 @@ void crsf_telem_set_battery_data(uint16_t voltage, uint16_t current, uint32_t ca
   _telemetry.battery_sensor.capacity = capacity;
   _telemetry.battery_sensor.percent = percent;
   frameHasData[CRSF_BATTERY_INDEX] = true;
-}
-
-void sb_reset(stream_buffer_t *sbuf)
-{
-  if (sbuf)
-  {
-    sbuf->offset = 0;
-  }
-}
-
-// Write an uint8_t to the buffer
-void sb_write_ui8(stream_buffer_t *sbuf, uint8_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(uint8_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = data;
-    sbuf->offset += sizeof(uint8_t);
-  }
-}
-
-// Write an int8_t to the buffer
-void sb_write_i8(stream_buffer_t *sbuf, int8_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(int8_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = data;
-    sbuf->offset += sizeof(int8_t);
-  }
-}
-
-// Write an uint16_t to the buffer
-void sb_write_ui16(stream_buffer_t *sbuf, uint16_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(uint16_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = data & 0xFF;
-    sbuf->offset += sizeof(uint16_t);
-  }
-}
-
-// Write an int16_t to the buffer
-void sb_write_i16(stream_buffer_t *sbuf, int16_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(int16_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = data & 0xFF;
-    sbuf->offset += sizeof(int16_t);
-  }
-}
-
-// Write an uint24_t to the buffer
-void sb_write_ui24(stream_buffer_t *sbuf, uint32_t data)
-{
-  if (sbuf && (sbuf->offset + 3 <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 16) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 2] = data & 0xFF;
-    sbuf->offset += 3;
-  }
-}
-
-// Write an int24_t to the buffer
-void sb_write_i24(stream_buffer_t *sbuf, int32_t data)
-{
-  if (sbuf && (sbuf->offset + 3 <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 16) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 2] = data & 0xFF;
-    sbuf->offset += 3;
-  }
-}
-
-// Write an uint32_t to the buffer
-void sb_write_ui32(stream_buffer_t *sbuf, uint32_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(uint32_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 24) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = (data >> 16) & 0xFF;
-    sbuf->buffer[sbuf->offset + 2] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 3] = data & 0xFF;
-    sbuf->offset += sizeof(uint32_t);
-  }
-}
-
-// Write an int32_t to the buffer
-void sb_write_i32(stream_buffer_t *sbuf, int32_t data)
-{
-  if (sbuf && (sbuf->offset + sizeof(int32_t) <= sbuf->capacity))
-  {
-    sbuf->buffer[sbuf->offset] = (data >> 24) & 0xFF;
-    sbuf->buffer[sbuf->offset + 1] = (data >> 16) & 0xFF;
-    sbuf->buffer[sbuf->offset + 2] = (data >> 8) & 0xFF;
-    sbuf->buffer[sbuf->offset + 3] = data & 0xFF;
-    sbuf->offset += sizeof(int32_t);
-  }
-}
-
-uint8_t crsf_crc8(const uint8_t *ptr, uint8_t len)
-{
-  static const uint8_t crsf_crc8tab[256] = {
-      0x00, 0xD5, 0x7F, 0xAA, 0xFE, 0x2B, 0x81, 0x54, 0x29, 0xFC, 0x56, 0x83, 0xD7, 0x02, 0xA8, 0x7D,
-      0x52, 0x87, 0x2D, 0xF8, 0xAC, 0x79, 0xD3, 0x06, 0x7B, 0xAE, 0x04, 0xD1, 0x85, 0x50, 0xFA, 0x2F,
-      0xA4, 0x71, 0xDB, 0x0E, 0x5A, 0x8F, 0x25, 0xF0, 0x8D, 0x58, 0xF2, 0x27, 0x73, 0xA6, 0x0C, 0xD9,
-      0xF6, 0x23, 0x89, 0x5C, 0x08, 0xDD, 0x77, 0xA2, 0xDF, 0x0A, 0xA0, 0x75, 0x21, 0xF4, 0x5E, 0x8B,
-      0x9D, 0x48, 0xE2, 0x37, 0x63, 0xB6, 0x1C, 0xC9, 0xB4, 0x61, 0xCB, 0x1E, 0x4A, 0x9F, 0x35, 0xE0,
-      0xCF, 0x1A, 0xB0, 0x65, 0x31, 0xE4, 0x4E, 0x9B, 0xE6, 0x33, 0x99, 0x4C, 0x18, 0xCD, 0x67, 0xB2,
-      0x39, 0xEC, 0x46, 0x93, 0xC7, 0x12, 0xB8, 0x6D, 0x10, 0xC5, 0x6F, 0xBA, 0xEE, 0x3B, 0x91, 0x44,
-      0x6B, 0xBE, 0x14, 0xC1, 0x95, 0x40, 0xEA, 0x3F, 0x42, 0x97, 0x3D, 0xE8, 0xBC, 0x69, 0xC3, 0x16,
-      0xEF, 0x3A, 0x90, 0x45, 0x11, 0xC4, 0x6E, 0xBB, 0xC6, 0x13, 0xB9, 0x6C, 0x38, 0xED, 0x47, 0x92,
-      0xBD, 0x68, 0xC2, 0x17, 0x43, 0x96, 0x3C, 0xE9, 0x94, 0x41, 0xEB, 0x3E, 0x6A, 0xBF, 0x15, 0xC0,
-      0x4B, 0x9E, 0x34, 0xE1, 0xB5, 0x60, 0xCA, 0x1F, 0x62, 0xB7, 0x1D, 0xC8, 0x9C, 0x49, 0xE3, 0x36,
-      0x19, 0xCC, 0x66, 0xB3, 0xE7, 0x32, 0x98, 0x4D, 0x30, 0xE5, 0x4F, 0x9A, 0xCE, 0x1B, 0xB1, 0x64,
-      0x72, 0xA7, 0x0D, 0xD8, 0x8C, 0x59, 0xF3, 0x26, 0x5B, 0x8E, 0x24, 0xF1, 0xA5, 0x70, 0xDA, 0x0F,
-      0x20, 0xF5, 0x5F, 0x8A, 0xDE, 0x0B, 0xA1, 0x74, 0x09, 0xDC, 0x76, 0xA3, 0xF7, 0x22, 0x88, 0x5D,
-      0xD6, 0x03, 0xA9, 0x7C, 0x28, 0xFD, 0x57, 0x82, 0xFF, 0x2A, 0x80, 0x55, 0x01, 0xD4, 0x7E, 0xAB,
-      0x84, 0x51, 0xFB, 0x2E, 0x7A, 0xAF, 0x05, 0xD0, 0xAD, 0x78, 0xD2, 0x07, 0x53, 0x86, 0x2C, 0xF9};
-
-  uint8_t crc = 0;
-  for (uint8_t i = 0; i < len; i++)
-  {
-    crc = crsf_crc8tab[crc ^ *ptr++];
-  }
-  return crc;
 }
